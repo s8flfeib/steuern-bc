15b05ce98787ca14e6f5f800ff7b7fa4d916fc3a
s8flfeib <florian.feibel@web.de>
2021-12-11 15:14:11 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// load the things we need
+const express = require("express")
+const app = express()
+
+//set view engin to ejs
+app.set('view engine', 'ejs');
+
+
+// provide static data in express
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+
+
+
+// use res.render to load up an ejs view file
+
+//index page
+app.get("/", (req, res) => {
+    res.render('index')
+})
+
+const PORT = process.env.PORT || 1000
+
+
+
+
+app.listen(PORT, () => {
+    console.log('Server running on port $(PORT)')
+})
